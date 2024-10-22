9a77a9c43671112294551a0320949939b1e4d2d3
JosephLucaSanJuan <jurassicjoe03@gmail.com>
2024-10-22 12:43:01 +0200

    cuarto_commit_(UiState)

--- app/src/main/java/com/example/pokedex/ui/MainActivityViewModel.kt
+++ app/src/main/java/com/example/pokedex/ui/MainActivityViewModel.kt
@@ -4,6 +4,9 @@ import androidx.lifecycle.ViewModel
 import com.example.pokedex.data.Pokemon
 import com.example.pokedex.data.PokemonRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
 import javax.inject.Inject
 
 @HiltViewModel
@@ -13,4 +16,14 @@ class MainActivityViewModel @Inject constructor(
     suspend fun read():List<Pokemon> {
         return repository.readAll()
     }
+
+    private val _uiState = MutableStateFlow<PokedexUiState>(PokedexUiState.Loading)
+    val uiState: StateFlow<PokedexUiState>
+        get() = _uiState.asStateFlow()
+}
+
+sealed class PokedexUiState {
+    data object Loading: PokedexUiState()
+    class Error(message:String):PokedexUiState()
+    class Success(val pokemon: List<Pokemon>)
 }
